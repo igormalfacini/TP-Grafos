a1620afef49418bbd7e89480887562e12bf240fc
Igor Malfacini <84872501+igormalfacini@users.noreply.github.com>
2023-07-16 22:42:15 -0300

    fix: Interação com o usuário.

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from grafoPonderado import GrafoPonderado
 
 g = GrafoPonderado()
-nome_planilha = "votacoesVotos-2023.xlsx"
-#input("Informe o arquivo de votações: ")
-g.ler_planilha_cria_nos(nome_planilha)
-g.ler_planilha_criar_aresta(nome_planilha)
-g.gerar_arquivo_qtd_votacoes_participadas(nome_planilha)
-g.gerar_arquivo_grafo("grafovotacao2023.txt")
-print(f"Processando...\nO grafo foi escrito nos arquivos:\n- votacaoVotos-2023-graph.txt\n- votacaoVotos-2023-deputados.txt")
+nome_planilha = input("Informe o arquivo de votações: ")
+arquivo_votacoes = g.gerar_arquivo_qtd_votacoes_participadas(nome_planilha)
+arquivo_grafo = g.gerar_arquivo_grafo(nome_planilha)
+print(f"Processando...\nO grafo foi escrito nos arquivos:\n- {arquivo_grafo}\n- {arquivo_votacoes}")
